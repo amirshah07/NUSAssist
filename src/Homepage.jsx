ccbe930a0fdcdfaf67b28e1d1aa1c11097cc8aef
Amir Shah <amirshah@Amirs-MacBook-Pro.local>
2025-05-16 02:27:15 +0800

    initial navbar and homepage creation

---
+++ src/Homepage.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import Navbar from "./Navbar";
+
+export default function Hompage() {
+    
+    return(
+        <>
+            <Navbar/>
+            <main className="homepage">
+                <form>
+                    <div>
+                        <label htmlFor="AY">Which AY are you planning for?</label>
+                        <input
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="sem">Which semester are you planning for?</label>
+                        <input
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="mods">What modules are you taking?</label>
+                        <input
+                            type="text"
+                        />
+                        <h2>Modules to appear here in point format with "x" to remove</h2>
+                    </div>
+                    <div>
+                        <label htmlFor="AY">What timeblocks will you like to be free?</label>
+                        <button>Add Timeblocks</button>
+                    </div>
+                    <div>
+                        <h2>Timeblocks to appear here in table format</h2>
+                    </div>
+                    <div className="homepage-submit">
+                        <button type="submit">Create Timetable</button>
+                    </div>
+                </form>
+            </main>
+        </>
+    )
+}
---
+++ src/Navbar.jsx
@@ -0,0 +1,33 @@
+import profile from "./assets/images/emptyprofile.jpg";
+
+export default function Navbar() {
+    return (
+        <header className="navbar">
+          <div className="navbar-container">
+            <div className="navbar-left">
+                <a href="/homepage">
+                <h1>NUSAssist</h1>
+                </a>
+            </div>
+
+            <div className="navbar-center">
+                <a href="/timetable">
+                My Timetable
+                </a>
+                <a href="/gpacalculator">
+                GPA Calculator
+                </a>
+                <a href="/roadmap">
+                University Roadmap
+                </a>
+            </div>
+
+            <div className="navbar-right">
+                <a href="/profile">
+                <img src={profile} alt="Profile"/>
+                </a>
+            </div>
+          </div>
+        </header>
+    );
+}
